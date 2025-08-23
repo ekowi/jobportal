3729376035b6baf2ec6bd119abb80e2ab80f4fc1
Tegar Witjaksono <tegarwtj2308@gmail.com>
2025-08-24 03:14:04 +0700

    Revert: Mengembalikan ke kondisi commit bdd712746c34f866d20054d61205693a01418892

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Lowongan;
 
 class JobController extends Controller
 {
@@ -14,7 +13,6 @@ class JobController extends Controller
 
     public function browse()
     {
-        $jobs = Lowongan::with('kategoriLowongan')->latest()->get();
-        return view('jobs.browse', compact('jobs'));
+        return view('jobs.browse');
     }
 }
